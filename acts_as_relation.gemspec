25845b08b3398be84ac02065ef7a5eeef627afef
Matt Solt <mattsolt@gmail.com>
2014-03-15 17:03:22 -0400

    reconfigure test suite not to depend on rails

--- acts_as_relation.gemspec
+++ acts_as_relation.gemspec
@@ -1,10 +1,10 @@
-# -*- encoding: utf-8 -*-
-$:.push File.expand_path('../lib', __FILE__)
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'version'
 
 Gem::Specification.new do |s|
 
-  # Description Meta...
   s.name        = 'acts_as_relation'
   s.version     = ActiveRecord::ActsAsRelation::VERSION
   s.platform    = Gem::Platform::RUBY
@@ -14,20 +14,19 @@ Gem::Specification.new do |s|
   s.summary     = 'Easy multi-table inheritance for rails'
   s.description = "This 'acts_as' extension provides multi-table inheritance for rails models."
 
-
-  # Load Paths...
-  s.files         = `git ls-files`.split("\n")
-  s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
-  s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
+  s.files         = `git ls-files -z`.split("\x0")
+  s.executables   = s.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  s.test_files    = s.files.grep(%r{^(test|spec|features)/})
   s.require_paths = ['lib']
 
+  s.add_dependency 'activerecord', '~> 4.0'
+  s.add_dependency 'activesupport', '~> 4.0'
 
-  # Dependencies (installed via 'bundle install')...
-  s.add_development_dependency "rake"
-  s.add_development_dependency "bundler"
-  s.add_development_dependency "sqlite3"
-  s.add_development_dependency "rspec-rails"
-  s.add_development_dependency "rails"
-  s.add_development_dependency "database_cleaner"
-  s.add_dependency "activerecord"
+  s.add_development_dependency 'rake'
+  s.add_development_dependency 'bundler'
+  s.add_development_dependency 'sqlite3'
+  s.add_development_dependency 'rspec'
+  s.add_development_dependency 'pry'
+
 end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,52 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../dummy/config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
+require 'rspec'
+require 'sqlite3'
+require 'active_record'
+require 'active_support'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+require 'acts_as_relation'
+
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: ":memory:"
+)
+
+logger = ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), "debug.log"))
+ActiveRecord::Base.default_timezone = :utc
+
+begin
+  old_logger_level, logger.level = logger.level, ::Logger::ERROR
+  ActiveRecord::Migration.verbose = false
+  # Load Schema
+  load(File.dirname(__FILE__) + '/schema.rb')
+  # Load Models
+  basedir = File.dirname(__FILE__) + '/models'
+  Dir["#{basedir}/*.rb"].each do |path|
+    name = "#{File.basename(path, '.rb')}"
+    autoload name.classify.to_sym, "#{basedir}/#{name}"
+  end
+ensure
+  logger.level = old_logger_level
+end
 
 RSpec.configure do |config|
-  config.infer_base_class_for_anonymous_controllers = false
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = 'random'
+
+  # Wrap each spec in a transaction to roll back all the database changes
+  # after each spec is run
+  config.around do |example|
+    ActiveRecord::Base.transaction do
+      example.run
+      raise ActiveRecord::Rollback
+    end
+  end
 end
+
+Dir[File.join(File.dirname(__FILE__), 'support', '**', '*.rb')].each { |f| require f }
